ad6e887ee4d305d485160d859477cb92b4ae2edc
M-sharkawy <mostafasharks021@gmail.com>
2025-04-24 18:17:32 +0200

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """function that returns a tuple. The first element of the tuple is 
+    """function that returns a tuple. The first element of the tuple is
     the string k. The second element is the square of the int/float v
     """
     return (k, v ** 2)
