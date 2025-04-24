9ee734387120e8f676c198ed83d767d58c309672
M-sharkawy <mostafasharks021@gmail.com>
2025-04-24 18:59:33 +0200

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Annotate the below function’s parameters
 and return values with the appropriate typesr"""
-from typing import List, Tuple, Sequence, Iterable, Callable
+from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """function for elements list"""
     return [(i, len(i)) for i in lst]
