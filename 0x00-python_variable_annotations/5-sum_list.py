be8b412e533e8f041853e20561d17bf3e715893d
M-sharkawy <mostafasharks021@gmail.com>
2025-04-24 17:49:35 +0200

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,6 +6,7 @@ type-annotated function sum_list which takes a list input_list of floats
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """
     type-annotated function sum_list which takes a list input_list of floats
