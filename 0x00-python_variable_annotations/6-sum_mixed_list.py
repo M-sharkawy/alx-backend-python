e3c54afc20ca5803c5825405a546896d9f12e0da
M-sharkawy <mostafasharks021@gmail.com>
2025-04-24 18:30:17 +0200

    task 8

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,4 +6,7 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    type-annotated function sum_mixed_list which takes a list mxd_lst
+    """
     return sum(mxd_lst)
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,9 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """a type-annotated function make_multiplier
+    that takes a float multiplier"""
     def multiplier_func(x: float) -> float:
+        """multiplie float with the multiplier"""
         return x * multiplier
     return multiplier_func
