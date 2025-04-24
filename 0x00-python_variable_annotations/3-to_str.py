06a5314304dd549967cc12ecf3806caba9556724
M-sharkawy <mostafasharks021@gmail.com>
2025-04-24 17:32:42 +0200

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function to_str that takes a float n"""
+
+
+def to_str(n: float) -> str:
+    """type-annotated function to_str that takes a float n"""
+    return str(n)
