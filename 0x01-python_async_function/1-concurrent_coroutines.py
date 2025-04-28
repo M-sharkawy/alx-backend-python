4e25f4cd2baac00acec11900cb61c4fc8dbfa726
M-sharkawy <mostafasharks021@gmail.com>
2025-04-28 20:31:24 +0300

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""model to spawn wait_random n times and return delays"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n, max_delay):
+    """Run wait_random n times and collect delays in ascending order"""
+    delays =[]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        results = await task
+        delays.append(results)
+
+    return delays
